95bbb6739f202629e7dfcea29584f1402c941f96
Yubei <394634397@qq.com>
2017-10-17 22:10:40 +0800

    add parse argus

--- homework/hw_2/selpg/mySelpg.go
+++ homework/hw_2/selpg/mySelpg.go
@@ -20,17 +20,18 @@ type argus struct {
 }
 
 func main()  {
-  var myArgus argus;
+  var myArgus argus
 
-  setArgus(&myArgus);
-  parseArgus(&myArgus);
-  fileProcess(&myArgs);
+  setArgus(&myArgus)
+  parseArgus(&myArgus)
+  fileProcess(&myArgus)
 }
 
+// input and set argus
 func setArgus(myArgus *argus)  {
-  myArgus.programName = os.Args[0]; // os
+  myArgus.programName = os.Args[0] // os
   flag.Usage = func() { // flag
-		fmt.Printf("Usage: %s -s=STARTPAGE -e=ENDPAGE [OPTION]... [FILE]...\n", myArgs.programName)
+		fmt.Printf("Usage: %s -s=STARTPAGE -e=ENDPAGE [OPTION]... [FILE]...\n", myArgus.programName)
 		fmt.Printf("Select specified pages from file or standard input.\n\n")
 		fmt.Printf("With no FILE, or when FILE is -, read standard input.\n\n")
 		fmt.Printf("\t-s=STARTPAGE\tPages number starts at STARTPAGE\n")
@@ -39,6 +40,7 @@ func setArgus(myArgus *argus)  {
 		fmt.Printf("\t-f\tInput file use 'f' to seperate two pages\n")
 		fmt.Printf("\t-d\tThe destination of output\n\n")
 	}
+
   flag.IntVar(&myArgus.startPage, "s", -1, "specify start page.")
 	flag.IntVar(&myArgus.endPage, "e", -1, "specify end page.")
 	flag.IntVar(&myArgus.pageLen, "l", -1, "specify page length(number of lines).")
@@ -46,5 +48,32 @@ func setArgus(myArgus *argus)  {
 	flag.StringVar(&myArgus.desPrint, "d", "", "specify the destination program.")
 
 	flag.Parse()
+}
 
+// parse argus
+func parseArgus(myArgus * argus) {
+  var programName = myArgus.programName
+  // startPage and endPage required
+  if myArgus.startPage < 0 || myArgus.endPage < 0 {
+		processError(proName, fmt.Sprintf("not enough arguments, -s and -e is indispensable."))
+	}
+  // startPage must be smaller than endPage
+  if myArgus.startPage > myArgus.endPage {
+    processError(proName, fmt.Sprintf("STARTPAGE(%d) should not be greater than ENDPAGE(%d).", myArgus.startPage, myArgus.endPage))
+  }
+
+  myArgus.srcFile = flag.Arg(0)
+
+  var defaultLength int = 72
+  if myArgus.pageType == true {
+    // ensure exclusion between '-f' and '-l'
+    if myArgus.pageLen != -1 {
+      processError(proName, "-f and -l=PAGELENGTH are mutually-exclusive.")
+    }
+  } else {
+    // assign a default value
+    if myArgus.pageLen < 1 {
+      myArgus.pageLen = defaultLength
+    }
+  }
 }
